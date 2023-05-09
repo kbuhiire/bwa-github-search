aab49363e5d296c158a33664e8e682bdf3c02a9f
Buhiire Keneth <buhiirekeneth@gmail.com>
2023-05-09 08:46:06 +0300

    disable redux logger in production

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,15 @@ import { configureStore } from '@reduxjs/toolkit'
 import userReducer from 'redux/user'
 import {createLogger} from 'redux-logger'
 
-const logger = createLogger()
+const middlewares = []
+if (process.env.NODE_ENV === 'development') {
+    const logger = createLogger()
+    middlewares.push(logger)
+}
+
 export default configureStore({
     reducer: {
         user: userReducer
     },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(middlewares),
 })
